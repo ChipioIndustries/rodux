f1c6e08fa3e01f4c4a7d4e21f66cfcaad53ffcef
Chipio Industries <53354220+ChipioIndustries@users.noreply.github.com>
2022-01-23 10:46:05 -0800

    Fix testing

--- test/server/TestExecution.server.lua
+++ test/server/TestExecution.server.lua
@@ -4,7 +4,7 @@ local TestEZ = require(ReplicatedStorage.DevPackages.TestEZ)
 
 TestEZ.TestBootstrap:run(
 	{
-		ReplicatedStorage.Rapscallion
+		ReplicatedStorage.Rodux
 	},
 	TestEZ.Reporters.TextReporter
 )
